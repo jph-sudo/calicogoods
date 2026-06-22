9e482a46fcff6e8f611fc296969cb76d06185e75
jph-sudo <josh.p.hoeflich@gmail.com>
2026-06-22 12:54:40 -0400

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,11 @@ export default function Navbar() {
   return (
     <nav className="flex items-center justify-between p-6">
       <Link href="/">
-        <img src="/logo.png" alt="Calico Goods Logo" className="h-12 w-auto" />
+        <img 
+          src="/logo.png" 
+          alt="Calico Goods Logo" 
+          className="h-10 w-auto" // Change h-10, h-12, or h-14 to find the best fit
+        />
       </Link>
       <div className="space-x-8 text-lg font-medium">
         <Link href="/">Home</Link>
