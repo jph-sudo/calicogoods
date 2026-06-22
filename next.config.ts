2379a76d4663239ea8174cebe16cdf768f9f661e
jph-sudo <josh.p.hoeflich@gmail.com>
2026-06-22 19:20:44 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export",  // Required for static export to GitHub Pages
   basePath: "",  // Adjust to your repository name
   images: {
     unoptimized: true, // Required for Next.js Image component on GitHub Pages
